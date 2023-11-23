ab491344bebc426d5881ae4becda9c87edd6fd1f
Matteo Naccarato <matteo.naccarato@itiscuneo.eu>
2023-11-23 23:55:05 +0100

    small refactor on errors management

--- DB/DB.php
+++ DB/DB.php
@@ -17,11 +17,15 @@ class DB {
         mysqli_report(MYSQLI_REPORT_OFF);
 
         $conn = new mysqli($ini["hostname"], $ini["username"], $ini["password"], $ini["database"], $ini["port"]);
-        if ($conn->connect_errno)
+        if ($conn->connect_errno) {
             error("500", "mysqli error: $conn->connect_errno", $source, $redirect_error);
+            exit;
+        }
         $conn->set_charset("utf8mb4");
-        if ($conn->errno)
+        if ($conn->errno) {
             error("500", "mysqli error: $conn->error", $source, $redirect_error);
+            exit;
+        }
 
         return $conn;
     }
@@ -30,39 +34,54 @@ class DB {
 
         $stmt = self::p_stmt_bind_execute($conn, $query, $type_params, $params, $source, $redirect_error);
 
-        if (!$res = $stmt->get_result())
+        if (!$res = $stmt->get_result()) {
             error("500", "get_result()", $source, $redirect_error);
-        if (!$stmt->close())
+            exit;
+        }
+        if (!$stmt->close()) {
             error("500", "stmt_close error: $stmt->error", $source, $redirect_error);
-        if (($element = $res->fetch_assoc()) === false)
+            exit;
+        }
+        if (($element = $res->fetch_assoc()) === false) {
             error("500", "fetch_assoc()", $source, $redirect_error);
+            exit;
+        }
         $res->free_result();
 
         return $element;
     }
 
     public static function stmt_no_select($conn, $query, $source = "N/A", $redirect_error = ""): void {
-        if (!$conn->query($query))
+        if (!$conn->query($query)) {
             error("500", "mysqli: query", $source, $redirect_error);
-
+            exit;
+        }
     }
 
     public static function p_stmt_no_select($conn, $query, $type_params, $params, $source = "N/A", $redirect_error = ""): void {
         $stmt = self::p_stmt_bind_execute($conn, $query, $type_params, $params, $source, $redirect_error);
 
-        if (!$stmt->close())
+        if (!$stmt->close()) {
             error("500", "stmt_close error: $stmt->error", $source, $redirect_error);
+            exit;
+        }
     }
 
     public static function p_stmt_bind_execute($conn, $query, $type_params, $params, $source = "", $redirect_error  = "") {
         $s_type_params = implode("", $type_params);
 
-        if (!$stmt = $conn->prepare($query))
+        if (!$stmt = $conn->prepare($query)) {
             error("500", "mysqli prepare: $conn->error", $source, $redirect_error);
-        if (!$stmt->bind_param($s_type_params, ...$params))
+            exit;
+        }
+        if (!$stmt->bind_param($s_type_params, ...$params)) {
             error("500", "mysqli bind_param: $conn->error", $source, $redirect_error);
-        if (!$stmt->execute())
+            exit;
+        }
+        if (!$stmt->execute()) {
             error("500", "stmt_execute error: $stmt->error", $source, $redirect_error);
+            exit;
+        }
         return $stmt;
     }
 }
