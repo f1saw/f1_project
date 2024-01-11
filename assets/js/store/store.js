f5160f2cead47ff7b455a98a34ccfa6c634f68ac
Matteo Naccarato <matteo.naccarato@itiscuneo.eu>
2024-01-11 14:46:33 +0100

    Fix products viewed on store

--- assets/js/store/store.js
+++ assets/js/store/store.js
@@ -1,8 +1,9 @@
 /** Render products management in pages in store.php */
 const LOWER_BOUND = 0;
-const UPPER_BOUND = 10;
+const UPPER_BOUND = 9;
 const ITEMS_PER_PAGE = UPPER_BOUND - LOWER_BOUND + 1;
 const render_products = (lower, upper) => {
+    console.log(lower + " " + upper)
     const products = $(".product").toArray();
     const prev_btn = $("#prev-page");
     const next_btn = $("#next-page");
