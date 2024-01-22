eab8ba6b3ea410eeed596bb30a6899bc6a9eecd0
Matteo Naccarato <matteo.naccarato@itiscuneo.eu>
2024-01-22 15:53:49 +0100

    Refactor error_handling

--- utility/error_handling.php
+++ utility/error_handling.php
@@ -1,18 +1,31 @@
 <?php
+if (!defined("BASE_INCLUDE_PATH")) {
+    // define("BASE_INCLUDE_PATH", "/chroot/home/S5330843/public_html/f1_project/"); // on SERVER
+    define("BASE_INCLUDE_PATH", $_SERVER["DOCUMENT_ROOT"]); // on LOCAL
+}
 if(session_status() == PHP_SESSION_NONE) session_start();
 
 $_DEFAULT_ERROR_REDIRECT = "/f1_project/views/public/index.php";
 
-function error($e_code = "", $e_msg = "", $source = "", $redirect = "", $e_usr_msg = "") : void {
+/**
+ * Custom function to handle errors generated executing PHP scripts
+ * @param string $e_code: error code
+ * @param string $e_msg: error message
+ * @param string $source: source file which has generated the error
+ * @param string $redirect: filepath where to redirect
+ * @param string $e_usr_msg: explicit error message (if needed)
+ * @return void
+ */
+function error(string $e_code = "", string $e_msg = "", string $source = "", string $redirect = "", string $e_usr_msg = "") : void {
     global $_DEFAULT_ERROR_REDIRECT;
 
-    $path = realpath($_SERVER["DOCUMENT_ROOT"]) . "\\$source";
+    $path = realpath(BASE_INCLUDE_PATH) . "\\$source";
     $e_json = "{\n" .
         "\t\"path\": \"$path\",\n" .
         "\t\"error_code\": \"$e_code\",\n " .
         "\t\"error_message\": \"$e_msg\"\n" .
         "},\n";
-    $path = $_SERVER["DOCUMENT_ROOT"] . "/errors.log";
+    $path = BASE_INCLUDE_PATH . "/errors.log";
     error_log($e_json, 3, $path);
 
     $_SESSION["err"] = 1;
