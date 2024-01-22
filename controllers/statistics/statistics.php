cffe671379413ac465661c80f17801420c4919a2
Matteo Naccarato <matteo.naccarato@itiscuneo.eu>
2024-01-22 13:15:56 +0100

    Refactor Statistics

--- controllers/statistics/statistics.php
+++ controllers/statistics/statistics.php
@@ -1,8 +1,7 @@
 <?php
 const BASE_URL_STATISTICS = "https://www.formula1.com/en/results.html/2023/races.html";
 
-function f1_scrape_stat($base_url): array
-{
+function f1_scrape_stat($base_url): array {
 
     // Init arrays of interest
     $info = [];
@@ -15,35 +14,36 @@ function f1_scrape_stat($base_url): array
     @$html->loadHtml($page);
     $xpath = new DOMXPath($html);
 
+    // Get Race Information
     $node_list = $xpath->query('//td[@class="dark bold"]');
     foreach ($node_list as $n) {
         $node = $n->nodeValue;
 
-        // frist element gp, second name_driver, third lastname_driver and ver
+        // [GP_Name, Driver_Name]
+        // e.g. Bahrain ; Max Verstappen VER
         $info[] = $node;
     }
 
+    // Get Race Date
     $node_list = $xpath->query('//td[@class="dark hide-for-mobile"]');
     foreach ($node_list as $n) {
         $node = $n->nodeValue;
-
         $date[] = $node;
     }
 
+    // Get Car Name
     $node_list = $xpath->query('//td[@class="semi-bold uppercase "]');
     foreach ($node_list as $n) {
         $node = $n->nodeValue;
-
         $car[] = $node;
     }
 
+    // Get Laps
     $node_list = $xpath->query('//td[@class="bold hide-for-mobile"]');
     foreach ($node_list as $n) {
         $node = $n->nodeValue;
-
         $laps[] = $node;
     }
-
 
     return [$info, $date, $car, $laps];
 }
