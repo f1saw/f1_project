0d2749d4f3e1fd4776edcedab44fc5609fa2670f
Matteo Naccarato <matteo.naccarato@itiscuneo.eu>
2024-01-22 21:38:00 +0100

    service

--- DB/DB.php
+++ DB/DB.php
@@ -150,7 +150,7 @@ class DB {
         }
         if (!$stmt->execute()) {
             if ($order_delete_id)
-                DB::stmt_no_select($conn, "DELETE FROM orders WHERE id = '$order_delete_id';", "DB.php", "/f1_project/views/public/store/cart.php");
+                DB::p_stmt_no_select($conn, "DELETE FROM orders WHERE id = ?;", ["i"], [$order_delete_id], "DB.php", "/f1_project/views/public/store/cart.php");
             error("500", "stmt_execute error: $stmt->error", $source, $redirect_error, $order_delete_id?"Something went wrong! Maybe one of your products is not available anymore.":"");
             exit;
         }
